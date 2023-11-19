aa66d83c149aee8c08864d19167be1d2ecb71ec7
shashanksriva <shashanksriva@gmail.com>
2023-11-18 22:53:44 -0500

    Effects to load data from local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { init } from './counter-store/counter.actions';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    this.store.dispatch(init());
+  }
+
+  constructor(private store: Store) {}
 }
--- src/app/counter-store/counter.effects.ts
+++ src/app/counter-store/counter.effects.ts
@@ -1,12 +1,26 @@
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { decrementCounter, incrementCounter } from './counter.actions';
-import { tap, withLatestFrom } from 'rxjs';
+import { decrementCounter, incrementCounter, init, set } from './counter.actions';
+import { of, switchMap, tap, withLatestFrom } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { selectCount } from './counter.selectors';
 
 @Injectable()
 export class CounterEffects {
+
+  loadCount = createEffect(() =>
+    this.actions$.pipe(
+      ofType(init),
+      switchMap(() => {
+        const storedCounter = localStorage.getItem('count');
+        if(storedCounter) {
+          return of(set({value: +storedCounter}));
+        }
+        return of(set({value: 0}));
+      })
+    )
+  );
+
   saveCount = createEffect(() =>
     this.actions$.pipe(
       ofType(incrementCounter, decrementCounter),
